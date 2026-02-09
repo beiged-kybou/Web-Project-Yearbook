b25acf38cf6280a919407ee4754bd4785e77c184
MubtasimSajid <mubtasimsajidahmedarian.11@gmail.com>
2026-02-09 22:52:00 +0600

    new routes made

--- backend/src/routes/memoryRoutes.js
+++ backend/src/routes/memoryRoutes.js
@@ -1,14 +1,5 @@
 import express from "express";
-import {
-  getAllMemories,
-  getMemoriesByStudent,
-  getMemoryById,
-} from "../controllers/memoryController.js";
 
 const router = express.Router();
-
-router.get("/", getAllMemories);
-router.get("/student/:studentId", getMemoriesByStudent);
-router.get("/:id", getMemoryById);
 
 export default router;
